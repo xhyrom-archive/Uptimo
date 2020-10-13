87de3568204eccd71efdfcec455c09c31926e86d
HyRo <generalkubo@gmail.com>
2020-10-13 14:51:08 +0200

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,21 +1,18 @@
 const db = require("quick.db")
 const axios = require("axios");
 
-u()
-function u() {
-  setInterval(() => {
-      var d = db.get("urls")
-      d.forEach(url => {
-        axios.get(url).then(() => console.log("Ping at " + Date.now() + " ("+url+")")).catch((e) => {
-        console.log("Unable ping " + url)
-        })
+setInterval(() => {
+    var d = db.get("urls")
+    d.forEach(url => {
+      axios.get(url).then(() => console.log("Ping at " + Date.now() + " ("+url+")")).catch((e) => {
+      console.log("Unable ping " + url)
       })
-  }, 60 * 1000)
-
-  var d = db.get("urls")
-  d.forEach(url => {
-    axios.get(url).then(() => console.log("Ping at " + Date.now() + " ("+url+")")).catch((e) => {
-    console.log("Unable ping " + url)
     })
+}, 60 * 1000)
+
+var d = db.get("urls")
+d.forEach(url => {
+  axios.get(url).then(() => console.log("Ping at " + Date.now() + " ("+url+")")).catch((e) => {
+  console.log("Unable ping " + url)
   })
-}
+})
