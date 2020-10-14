0dc71819e70ed48267e973db20b8573e45a7e8dc
HyRo <generalkubo@gmail.com>
2020-10-14 14:54:38 +0200

    Added dashboard remove system

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const db = require("quick.db")
+const bcrypt = require('bcrypt')
 const express = require("express")
 const app = express()
 const first = db.get("first")
@@ -27,35 +28,91 @@ app.get("/", async(req, res) => {
   })
 })
 
-app.get("/admin", async(req, res) => {
+/* LOGIN */
+app.post("/register", async(req, res) => {
   var i = db.get("urls")
-  res.render("admin", {
-    has: i.length
+  var name = req.body.name
+  var pass = req.body.pass
+  var acc = db.get(`account_${name}`)
+
+  if(acc) return res.render("error", {
+    error: true,
+    status: 400,
+    error: "Account already exists!"
+  }) 
+
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(pass, salt)
+
+  db.set(`account_${name}`, {
+    pass: hash,
+    name: name,
+    ban: false
+  })
+
+  if(!acc) return res.render("error", {
+    error: false,
+    status: 200,
+    error: "Account succesfully created!"
   })
 })
 
 /* LOGIN */
 app.post("/login", async(req, res) => {
   var i = db.get("urls")
+  var u = db.all().filter(data => data.ID.startsWith(`account`)).sort((a, b) => b.data - a.data)
+
+  var ir = 0;
+  var g = "";
+  for (ir in u) {
+    g += `${u[ir].ID.split('_')[1]} <a style="background: transparent;" href="/b?pass=${req.body.pass}&user=${u[ir].ID.split('_')[1]}&type=ban">BAN</a> | <a style="background: transparent;" href="/b?pass=${req.body.pass}&user=${u[ir].ID.split('_')[1]}&type=delete">DELETE</a><br>`;
+  }
+
+  var ur = ""
+  i.forEach(function(url) {
+    ur += `${url} <a style="background: transparent;" href="/r?pass=${req.body.pass}&url=${url}">DELETE</a><br>`
+  })
+
   var name = req.body.name
   var pass = req.body.pass
+  var acc = db.get(`account_${name}`)
 
-  if(name !== process.env.name) return res.render("error", {
+
+  if(!acc) return res.render("error", {
     error: true,
     status: 400,
-    error: "Please check username. Username is bad"
+    error: "Account not exists"
   }) 
 
-  if(pass !== process.env.pass) return res.render("error", {
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(pass, salt)
+
+  const match = await bcrypt.compare(pass, acc.pass);
+
+  if(!match) return res.render("error", {
     error: true,
     status: 400,
     error: "Please check password. Password is bad"
-  }) 
+  })
+
+  if(acc.ban) return res.render("error", {
+    error: true,
+    status: 400,
+    error: "Your account disabled :("
+  })
+
+  var perms;
+  if(acc.name === process.env.adminname) {
+    perms = true
+  } else {
+    perms = false
+  }
 
   res.render("dashboard", {
+    perms: perms,
+    urls: ur,
     has: i.length,
-    urls: i,
-    key: pass
+    members: g
   })
 })
 
@@ -96,32 +153,94 @@ app.post("/create", async(req, res) => {
   })
 })
 
-app.post("/remove", async(req, res) => {
-  var u = db.get("urls")
-  var url = req.body.ur
-  var key = req.body.key
+/* BAN USER & REMOVE URL */
+app.get("/b", async(req, res) => {
+  const acc = db.get(`account_${process.env.adminname}`)
 
-  if(!url) return res.render("error", {
+  if(!req.query.pass) return res.render("error", {
+    error: true,
+    status: 400,
+    error: "Please define password."
+  })
+
+  const match = await bcrypt.compare(req.query.pass, acc.pass);
+  if(!match) return res.render("error", {
+    error: true,
+    status: 400,
+    error: "Please check password. Password is bad"
+  })
+
+  if(!req.query.user) return res.render("error", {
+    error: true,
+    status: 400,
+    error: "Please define user."
+  })
+
+  var del = db.get(`account_${req.query.user}`)
+  if(!del) return res.render("error", {
+    error: true,
+    status: 400,
+    error: "Please check user. User is bad"
+  })
+
+
+  if(req.query.type === "ban") {
+    var old = db.get(`account_${req.query.user}`)
+    db.set(`account_${req.query.user}`, {
+      pass: old.pass,
+      name: old.name,
+      ban: true
+    })
+
+    return res.render("error", {
+        error: false,
+        status: 200,
+        error: "User succesfully banned!"
+    })
+  }
+
+  if(req.query.type === "delete") {
+    db.delete(`account_${req.query.user}`)
+
+    return res.render("error", {
+        error: false,
+        status: 200,
+        error: "User succesfully deleted!"
+    })
+  }
+})
+
+app.get("/r", async(req, res) => {
+  const acc = db.get(`account_${process.env.adminname}`)
+  const u = db.get("urls")
+
+  if(!req.query.pass) return res.render("error", {
+    error: true,
+    status: 400,
+    error: "Please define password."
+  })
+  const match = await bcrypt.compare(req.query.pass, acc.pass);
+  if(!match) return res.render("error", {
+    error: true,
+    status: 400,
+    error: "Please check password. Password is bad"
+  })
+
+  if(!req.query.url) return res.render("error", {
     error: true,
     status: 400,
     error: "Please define url."
-  }) 
+  })
 
-  if(key !== process.env.pass) return res.render("error", {
-    error: true,
-    status: 400,
-    error: "Please check key. Key is bad"
-  }) 
-
-  if (u.indexOf(url) > -1) {
+  if (u.indexOf(req.query.url) > -1) {
     var array = db.get("urls");
-    array = array.filter(v => v !== url);
+    array = array.filter(v => v !== req.query.url);
     db.set("urls", array)
 
     res.render("error", {
         error: false,
         status: 200,
-        error: "URL is deleted! (" + url + ")"
+        error: "URL is deleted! (" + req.query.url + ")"
     }) 
   return;
   }
