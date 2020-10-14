cee354e24b2e0efd72cc98e34c49add53620a7c6
HyRo <generalkubo@gmail.com>
2020-10-14 17:25:52 +0200

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,17 +1,21 @@
 const db = require("quick.db")
-const axios = require("axios");
+const fetch = require("node-fetch")
 const d = db.get("urls")
 
 setInterval(() => {
-    d.forEach(url => {
-      axios.get(url).then(() => console.log("✅ - Succesfully pinged (" + url + ")")).catch((e) => {
-      console.log("📛 - Failed ping (" + url + ")")
-      })
+  d.forEach(url => {
+    fetch(url).then(() => {
+      console.log("✅ - Succesfully pinged (" + url + ")")
+    }).catch((e) => {
+      console.log("📛 Failed ping (" + url + ")")
     })
+  })
 }, 60 * 1000)
 
 d.forEach(url => {
-  axios.get(url).then(() => console.log("✅ - Succesfully pinged (" + url + ")")).catch((e) => {
-  console.log("📛 - Failed ping (" + url + ")")
+  fetch(url).then(() => {
+    console.log("✅ - Succesfully pinged (" + url + ")")
+  }).catch((e) => {
+    console.log("📛 Failed ping (" + url + ")")
   })
 })
