a3240758229cf30fe7ce7ade0ab99e45ea03d8b1
HyRo <generalkubo@gmail.com>
2020-10-17 11:07:52 +0200

    Added check status

---
+++ api/check.js
@@ -0,0 +1,51 @@
+module.exports = async(app, db) => {
+  app.get("/api/check", async(req, res) => {
+    if(!req.query.url) return res.json({
+      status: 400,
+      error: "Please define url"
+    })
+
+    var url = req.query.url
+    var u = db.get("urls")
+
+    function isValidUrl(string) {
+      try {
+        new URL(string);
+      } catch (_) {
+        return false;  
+      }
+
+      return true;
+    }
+
+    if(isValidUrl(url) !== true || url.includes("<" || ">" || "<script>" || "</script>") || encodeURIComponent(url).includes("%3C" || "%3E" || "%20")) return res.json({
+      status: 400,
+      error: "Please check url. Url is not valid"
+    })
+
+    var ui = [];
+    var ur = ""
+    u.forEach(function(url) {
+      var ugall = url.split("<")[0]
+      ui.push(ugall)
+    })
+
+    if (ui.indexOf(url) > -1) {
+      var status = db.get(`status_${url}`)
+      var check = {
+        true: "online",
+        false: "offline",
+        undefined: "Please wait to check (1 minute)"
+      }
+
+      return res.json({
+        status: check[status]
+      })
+    }
+
+    return res.json({
+      status: 400,
+      error: "Please check url. Url is not on db"
+    }) 
+  })
+}
