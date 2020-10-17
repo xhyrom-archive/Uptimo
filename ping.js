5d7bf37c8d4d1b4c46f707652addba61c6f37f95
HyRo <generalkubo@gmail.com>
2020-10-17 12:07:32 +0200

    Added status

--- ping.js
+++ ping.js
@@ -2,15 +2,23 @@ const db = require("quick.db")
 const fetch = require("node-fetch")
 const d = db.get("urls")
 
+var check = {
+  true: "online",
+  false: "offline"
+}
+
 setInterval(() => {
   d.forEach(url => {
     var r = url.split("<")[0]
     fetch(r).then(() => {
+      var status = db.get(`status_${r}`)
       db.set(`status_${r}`, true)
-      console.log("✅ - Succesfully pinged (" + r + ")")
+
+      console.log(`✅ Succesfully pinged (${r}) | Status: ${check[status]}`)
     }).catch((e) => {
+      var status = db.get(`status_${r}`)
       db.set(`status_${r}`, false)
-      console.log("📛 Failed ping (" + r + ")")
+      console.log(`📛 Failed ping (${r}) | Status: ${check[status]}`)
     })
   })
 }, 60 * 1000)
@@ -18,10 +26,12 @@ setInterval(() => {
 d.forEach(url => {
   var r = url.split("<")[0]
   fetch(r).then(() => {
+    var status = db.get(`status_${r}`)
     db.set(`status_${r}`, true)
-    console.log("✅ - Succesfully pinged (" + r + ")")
+    console.log(`✅ Succesfully pinged (${r}) | Status: ${check[status]}`)
   }).catch((e) => {
+    var status = db.get(`status_${r}`)
     db.set(`status_${r}`, false)
-    console.log("📛 Failed ping (" + r + ")")
+    console.log(`📛 Failed ping (${r}) | Status: ${check[status]}`)
   })
 })
