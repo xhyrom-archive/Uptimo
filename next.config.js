2513d9e5a4371cea06ead7eafd641e0e25d37d09
xhyrom <generalkubo@gmail.com>
2022-01-02 14:57:19 +0100

    custom server

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
+const { createSecureHeaders } = require("next-secure-headers");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [{
+      source: "/(.*)",
+      headers: createSecureHeaders({
+        contentSecurityPolicy: {
+          directives: {
+            defaultSrc: "'self'",
+            styleSrc: ["'self'", "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"],
+          },
+        },
+        forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true }],
+        referrerPolicy: "same-origin",
+      }),
+    }];
+  }
 }
