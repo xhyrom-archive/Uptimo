f4a63e48b3ca69236bd98a1aff0ff4ce11a26723
xhyrom <generalkubo@gmail.com>
2022-01-02 18:40:30 +0100

    fix style, js

--- next.config.js
+++ next.config.js
@@ -7,12 +7,6 @@ module.exports = {
     return [{
       source: "/(.*)",
       headers: createSecureHeaders({
-        contentSecurityPolicy: {
-          directives: {
-            defaultSrc: "'self'",
-            styleSrc: ["'self'", "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"],
-          },
-        },
         forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true }],
         referrerPolicy: "same-origin",
       }),
